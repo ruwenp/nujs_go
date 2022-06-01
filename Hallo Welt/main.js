4d4ef6c02bf5154608a0eff5f0986ad111278440
ruwen <pruwen@gmail.com>
2022-06-01 13:39:17 +0200

    diverse

--- Hallo Welt/main.js
+++ Hallo Welt/main.js
@@ -1,3 +1,20 @@
 "use strict";
 
 console.log("hallo welt");
+const minalter = 18;
+let alter = prompt('alter eingeben:');
+let msg = 'zu jung';
+
+if( parseInt( alter ) > minalter  ) {
+    msg = 'alt genug';
+} 
+else if ( parseInt( alter ) === minalter ) {
+    msg = 'grade so ...';
+}
+
+const meine_func = function (){
+    console.log("hallo Func");
+};
+
+console.log(msg);
+meine_func();
--- liqui-planner/src/js/main.js
+++ liqui-planner/src/js/main.js
@@ -1,15 +1,34 @@
 "use strict";
 
-let titel = prompt("Titel");
-let type = prompt("Type: Einnahme/Ausgabe");
-let betrag = prompt("Betrag");
-let datum = prompt("Datum", "JAHR-MM-TT");
-
-console.log( "titel: " + titel + " / type: " + type);
-console.log( "Betrag: " + betrag + " / datum: " + datum);
-
 let einnahmen = 0;
 let ausgaben = 0;
+
+while (true) {
+    let titel = prompt("Titel");
+    let type = prompt("Type: Einnahme/Ausgabe");
+    let betrag = prompt("Betrag");
+    let datum = prompt("Datum", "JAHR-MM-TT");
+
+    console.log( `
+        titel: ${titel} 
+        type: ${type} 
+        Betrag: ${betrag} 
+        datum: ${datum}
+    `);
+
+    if ( type === 'Einnahme' ) {
+        einnahmen += parseInt(betrag);
+    } else {
+        ausgaben += parseInt(betrag);
+    }
+
+    let mehr_input_frage = prompt("Noch eine Angabe? j/n");
+    if ( mehr_input_frage !== 'j' ) {
+        break;
+    }
+
+}
+
 let bilanz = einnahmen - ausgaben;
 
-console.log( "Einnahmen: " + einnahmen + " - Ausgaben: " + ausgaben + ' = ' + bilanz);
+console.log( `Einnahmen: ${einnahmen} - Ausgaben: ${ausgaben} = ${bilanz}`);
